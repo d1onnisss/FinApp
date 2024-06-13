97b478324aeb3e6caa813df47c7e19c7cd8dc821
d1onnisss <lim_al@auca.kg>
2024-06-13 19:14:36 +0600

    Initial Commit

---
+++ FinApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinApp
+//
+//  Created by Alexey Lim on 13/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
